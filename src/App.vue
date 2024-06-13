96475f608fac59cedf49e2b07b00990476802be7
Blake Herbert <Bwherb1@gmail.com>
2024-06-13 18:33:32 -0400

    First commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloVue from './components/HelloVue.vue'
+</script>
+
+<template>
+  <h1>
+    <div class="wrapper">
+      <HelloVue msg="Hello Vue!" />
+    </div>
+  </h1>
+  <main></main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
---
+++ src/components/HelloVue.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
